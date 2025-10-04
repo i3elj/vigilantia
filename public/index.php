9a2ed0ff4a5798c78c590b4215811db232596890
r-odriguez <guisande.bernardo@gmail.com>
2025-10-04 13:54:06 -0300

    report and crimes api routes

--- public/index.php
+++ public/index.php
@@ -1,11 +1,15 @@
 <?php declare(strict_types=1);
 
 use Agmen\Router;
-use handlers\HomeHandler;
+use Handlers\CrimesApiHandler;
+use Handlers\HomeHandler;
+use Handlers\ReportHandler;
 
 require "../config.php";
 require "../vendor/autoload.php";
 
 const r = new Router();
 r->path("/", "home", [], HomeHandler::class);
+r->path("/report", "report", [], ReportHandler::class);
+r->path("/crimes", "crimes", [], CrimesApiHandler::class);
 r->handle();
