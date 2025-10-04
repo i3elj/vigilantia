ce5701f72780e6bb671f75b33ac86437fb70dd00
r-odriguez <guisande.bernardo@gmail.com>
2025-10-04 13:52:51 -0300

    adding some libraries

--- src/views/globals/head.php
+++ src/views/globals/head.php
@@ -3,15 +3,26 @@
 	<head>
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<title><?= $title ?? "Default Title" ?></title>
-		<link href="/css/output.css" rel="stylesheet"/>
-		<script defer type="module" src="/js/main.js"></script>
 
-		<?php foreach ($styles as $style): ?>
+		<!-- LEAFLET -->
+		<!-- css -->
+		<link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css"
+			integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY="
+			crossorigin=""/>
+
+		<script src="https://cdn.jsdelivr.net/npm/@phosphor-icons/web@2.1.2"></script>
+
+		<link href="/css/output.css" rel="stylesheet"/>
+		<script defer type="module" src="/js/imports.js"></script>
+
+		<?php foreach ($styles ?? [] as $style): ?>
 			<link href="<?= $style ?>" rel="stylesheet"/>
 		<?php endforeach; ?>
 
-		<?php foreach ($scripts as $script): ?>
+		<?php foreach ($scripts ?? [] as $script): ?>
 			<script defer type="module" src="<?= $script ?>"></script>
 		<?php endforeach; ?>
 	</head>
-	<body class="bg-base-200 min-w-screen min-h-screen">
+
+	<body class="flex items-center justify-center bg-base-200 min-w-screen min-h-screen">
+		<div id="notification-container" class="absolute top-sm right-sm z-99"></div>
